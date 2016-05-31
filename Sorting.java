d64c89a81b60f4d7fce92cb3fdfe3feeadb3009f
psdewar2 <psdewar2@live.com>
2016-05-30 23:44:36 -0400

    19th commit

--- Sorting.java
+++ Sorting.java
@@ -3,93 +3,135 @@ import java.util.*;
 public class Sorting {
 
 	public static void main(String[] args) {
-		int[] array = {4,5,3,9,1};
-		partition(array);
-		//System.out.println(partition(array));
-	}
-	
-	//MergeSort
-	//QuickSort
-	static void partition(int[] ar) {
-		//not in-place and uses Linked Lists (for now)
-		int p = ar[0];
-		List<Integer>lessAr = new LinkedList<Integer>();
-		List<Integer>greatAr = new LinkedList<Integer>();
-		for(int i=1; i<ar.length; i++){
-			if(ar[i]<p)
-				lessAr.add(ar[i]);
-			else
-				greatAr.add(ar[i]);
-		}
-		lessAr.add(p); //add pivot
-		lessAr.addAll(greatAr); //add 2nd half
-		//print
-		for(int n: lessAr){
-			System.out.print(n+" ");
-		}
-		System.out.println("");
+		Blob[] array = new Blob[20];
+		array[0] = new Blob(0, "ab");
+		array[1] = new Blob(6, "cd");
+		array[2] = new Blob(0, "ef");
+		array[3] = new Blob(6, "gh");
+		array[4] = new Blob(4, "ij");
+		array[5] = new Blob(0, "ab");
+		array[6] = new Blob(6, "cd");
+		array[7] = new Blob(0, "ef");
+		array[8] = new Blob(6, "gh");
+		array[9] = new Blob(0, "ij");
+		array[10] = new Blob(4, "that");
+		array[11] = new Blob(3, "be");
+		array[12] = new Blob(0, "to");
+		array[13] = new Blob(1, "be");
+		array[14] = new Blob(5, "question");
+		array[15] = new Blob(1, "or");
+		array[16] = new Blob(2, "not");
+		array[17] = new Blob(4, "is");
+		array[18] = new Blob(2, "to");
+		array[19] = new Blob(4, "the");
+		fullCountingSort(array);
 	}
 
-	//CountingSort
-	static void countingSort(int[] ar) { //for an array of numbers 0-99
-		//for any value, find the maximum value in the array, then add one for the length of the new array
-		int[] countingArray = new int[ar.length];
-		int i;
-		for (i = 0; i < countingArray.length; i++) {
-			//ar[i] is value in ar
-			countingArray[ar[i]]++;
-		}
-		int[] countingArray2 = new int[ar.length]; //for 1000 
-		int indexForCA2 = 0;
-		for (i = 0; i < countingArray.length; i++) {
-			if (countingArray[i] == 0) continue;
-			while (countingArray[i] > 0) {
-				countingArray2[indexForCA2] = i;
-				indexForCA2++;
-				countingArray[i]--;
+	//Full counting sort (for extra data)
+	static void fullCountingSort(Blob[] ar) {
+		int[] c = new int[100];
+        for (int i = 0; i < ar.length; i++) {
+        	c[ar[i].num]++;
+        }
+        int sum = 0;
+        for (int i = 0; i < c.length; i++) {
+        	sum += c[i];
+        	c[i] = sum;
+        }
+        
+        String[] newArray = new String[ar.length];
+        int cIndex = 0;
+        int blobIndex = 0;
+        for (int i = 0; i < ar.length; i++) {
+			//accessing original array
+			while (i < ar.length && blobIndex < c[cIndex]) {
+				if (ar[i].num == cIndex) {
+					newArray[blobIndex] = ar[i].string;
+					blobIndex++;
+				}
+				i++;
 			}
+			cIndex++;
+			if (cIndex > c.length - 1 || blobIndex > ar.length - 1) break;
+			i = -1;
 		}
+        
+        for (String str : newArray) {
+        	System.out.print(str + " ");
+        }
+        System.out.println();
 
-		for (int val : countingArray2) {
+	}
+
+	//Counting Sort
+	static void countingSort(int[] array) {
+		int max = array[0];
+		//if range of values is known 
+		//initialize array of size max + 1
+		//if not, traverse array to find max then init array
+		for (int val : array) {
+			max = Math.max(val, max);
+		}
+		int[] c = new int[max + 1];
+		for (int i = 0; i < array.length; i++) {
+			c[array[i]]++; //c
+		}
+		int sum = 0;
+		for (int i = 0; i < c.length; i++) {
+			sum += c[i];
+			c[i] = sum; //c'
+		}
+		int[] newArray = new int[array.length];
+
+		for (int i = 0; i < array.length; i++) {
+			//accessing original array
+			c[array[i]]--;
+			newArray[c[array[i]]] = array[i];
+		}
+		for (int val : newArray) {
 			System.out.print(val + " ");
 		}
+
 	}
 
-	//InsertionSort
-
-	static void iS(int[] ar) {
-		int rightmost = ar[ar.length - 1];
-		int i;
-		for (i = ar.length - 1; i > 0; i--) {
-			if (rightmost < ar[i-1]) {
-				ar[i] = ar[i-1];
-			} else {
-				break;
-			}
-			System.out.println(Arrays.toString(ar));
-			
-		}
-		ar[i] = rightmost;
-		System.out.println(Arrays.toString(ar));
-	}
-
-	static int insertionSort(int[] ar) {
-		int shifts = 0;
-		for (int i = 1; i < ar.length; i++) {
+	//Insertion Sort (descending order)
+	static void insertionSortDesc(int[] array) {
+		for (int i = 1; i <= array.length - 1; i++) {
 			for (int j = i; j > 0; j--) {
-				if (ar[j] < ar[j-1]) {
-					int temp = ar[j];
-					ar[j] = ar[j-1];
-					ar[j-1] = temp;
-					shifts++;
+				if (array[j] > array[j-1]) { //THE ONLY THING THAT CHANGES : >
+					int temp = array[j];
+					array[j] = array[j-1];
+					array[j-1] = temp;
+				} else {
+					break;
 				}
 			}
 		}
-		return shifts;
 	}
 
-	//1 4 3 6 9 2
-	//1 3 4 6 9 2
-	//
+	//Insertion Sort (ascending order)
+	static void insertionSortAsc(int[] array) {
+		for (int i = 1; i <= array.length - 1; i++) {
+			for (int j = i; j > 0; j--) {
+				if (array[j] < array[j-1]) {
+					int temp = array[j];
+					array[j] = array[j-1];
+					array[j-1] = temp;
+				} else { //since the values before index j-1 are sorted,
+					break; //makes sorting faster because it doesn't check values unnecessarily
+				}
+			}
+		}
+	}
+}
+
+class Blob{
+	String string;
+	int num;
 	
+	public Blob(int n, String str) 
+	{
+		num=n;
+		string=str;
+	}	
 }
